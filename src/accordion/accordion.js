8e00b124d3fbf9e3c366db8e6da05943d41367a5
mook <mook9288@gmail.com>
2019-04-20 23:35:20 +0900

    Accordion 기본 기능 완료..

--- src/accordion/accordion.js
+++ src/accordion/accordion.js
@@ -22,8 +22,8 @@ var old = null;
 
 function init() {
   initVars();
-  // initEvents();
-  // show(headers[defaultHeaderID]);
+  initEvents();
+  show(headers[defaultHeaderID]);
 }
 
 function initVars() {
@@ -33,12 +33,54 @@ function initVars() {
     var accContentID = el.getAttribute("href");
     var accContentEl = document.querySelector(accContentID);
     
+    if (!accContentEl) {
+      throw Error("컨텐츠가 없다!!!");
+    }
+
     headers[accContentID] = {
       header: el,
       content: accContentEl,
       index: i
     }
-    console.log(headers[accContentID])
+    // console.log(headers[accContentID])
+    console.log(defaultIdx)
+    
+    if (defaultIdx === i) {
+      defaultHeaderID = accContentID;
+    } else {
+      accContentEl.style.display = "none";
+    }
   })
 }
+
+function initEvents() {
+  for (var key in headers) {
+    var obj = headers[key];
+    var header = obj.header;
+    
+    header.addEventListener("click", function (e) {
+      e.preventDefault();
+      var target = e.currentTarget.getAttribute("href");
+      var me = headers[target];
+      show(me);
+      console.log(me)
+    });
+  }
+  
+}
+
+function show(target) {
+  if (old) {
+    hide(old);
+  }
+  classes.add(target.header,"active");
+  target.content.style.display = "block";
+  old = target;
+}
+
+function hide(target) {
+  classes.remove(target.header,"active");
+  target.content.style.display = "none";
+}
+
 init();
