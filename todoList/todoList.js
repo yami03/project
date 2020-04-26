71cf1b550830445d9d65600dd1688a75092ff72e
mook <mook9288@gmail.com>
2020-04-26 22:51:26 +0900

    바꾸는중

--- todoList/todoList.js
+++ todoList/todoList.js
@@ -99,89 +99,185 @@ function removeList(e) {
     console.log("선택한 항목이 삭제 되었습니다.");
   }
 }
-
-// 리스트 항목 완료
-function completedList() {
-  let chk = false;
-
-  document.querySelectorAll("input[type=checkbox]").forEach( e => {
-    if (e.checked === true) {
-      chk = true;
-      e.parentNode.parentNode.parentNode.classList.add("completed");
+window.onload = function () {
+  const btnAdd = document.querySelector('.btn-add');
+  btnAdd.addEventListener('click', function () {
+    const inputValue = document.querySelector(".opt-input").value;
+    if (inputValue === '') {
+      alert("내용을 입력하세요.");
+    } else {
+      alert("입력한 내용이 저장 되었습니다.");
+      document.querySelector(".opt-input").value = '';
+      document.querySelector(".todo-lst").innerHTML += `
+        <li>
+          <div class="txt">`+ inputValue + `</div>
+          <div class="btn">
+            <button class="btn-modify">수정</button>
+          </div>
+        </li>
+      `;
     }
+  }, false);
+
+  let flag = false;
+  
+  // 편집
+  const btnEdit = document.querySelector('.btn-edit');
+  btnEdit.addEventListener('click', function (e) {
+    const target = e.target;
+
+    if (!flag) {
+      flag = true;
+      target.innerText="취소";
+      document.querySelectorAll(".edit-group").forEach( e => {
+        e.classList.remove("hide");
+      });
+    } else {
+      flag = false;
+      target.innerText="편집";
+      document.querySelectorAll(".edit-group").forEach( e => {
+        e.classList.add("hide");
+      });
+    }
+
+    document.querySelectorAll(".todo-lst li").forEach( e => {
+      const txt = e.children[0];
+      const btn = e.children[1];
+      if (flag) {
+        txt.innerHTML = '<label><input type="checkbox" name="chkList" />' + txt.innerText + "</label>";
+        btn.classList.add("hide");
+      } else {
+        txt.innerHTML = txt.innerText;
+        btn.classList.remove("hide");
+      }
+    });
   });
 
-  if (!chk) {
-    alert("선택된 항목이 없습니다.");
-  } else {
-    chkboxHide();
-    console.log("선택한 항목이 완료 되었습니다.");
-  }
-}
-
-// 리스트 항목 진행
-function activeList() {
-  let chk = false;
-
-  document.querySelectorAll("input[type=checkbox]").forEach( e => {
-    if (e.checked === true) {
-      chk = true;
-      e.parentNode.parentNode.parentNode.classList.remove("completed");
-    }
-  });
-
-  if (!chk) {
-    alert("선택된 항목이 없습니다.");
-  } else {
-    chkboxHide();
-    console.log("선택한 항목이 진행 상태로 전환 되었습니다.");
-  }
-}
-
-// 리스트 수정
-let defaultValue;
-function modifyList(e) {
-  const modifyTxt = e.parentNode.parentNode.children[0];
-
-  if (!flag) {
-    flag = true;
-    e.innerText="저장";
-    e.parentNode.parentNode.classList.add("modify");
-    document.querySelector(".btn-edit").removeAttribute("enabled", "");
-    document.querySelector(".btn-edit").setAttribute("disabled", "");
-    
-    var node = document.createElement("button");
-    var nodeTxt = document.createTextNode("취소");
-    var btnCancle = e.parentNode.appendChild(node);
-    node.appendChild(nodeTxt);
-
-    if (btnCancle.getAttribute('onclick')) { //IE전용 객체 새로생성
-      btnCancle.onclick = function(){ cancleBtn(this); }
-    } else {//IE 외 브라우저
-      btnCancle.setAttribute('onclick','javascript:cancleBtn(this);');
-    }
-    defaultValue = modifyTxt.innerText;
-    modifyTxt.innerHTML = '<input type="text" value="' + modifyTxt.innerText + '" />';
-  } else {
+  // checkbox Hide
+  function chkboxHide() {
     flag = false;
-    e.innerText="수정";
-    e.nextElementSibling.remove();
-    e.parentNode.parentNode.classList.remove("modify");
-    document.querySelector(".btn-edit").setAttribute("enabled", "");
-    document.querySelector(".btn-edit").removeAttribute("disabled", "");
-    
-    const modifyInp = e.parentNode.parentNode.querySelector("input").value;
-    modifyTxt.innerHTML = modifyInp;
-  }
-}
+    document.querySelector(".btn-edit").innerText = "편집";
+    document.querySelectorAll(".edit-group").forEach( e => {
+      e.classList.add("hide");
+    });
 
-function cancleBtn(e) {
-  const modifyTxt = e.parentNode.parentNode.children[0];
-  flag = false;
-  e.previousElementSibling.innerText="수정";
-  e.parentNode.parentNode.classList.remove("modify")
-  e.remove();
-  document.querySelector(".btn-edit").setAttribute("enabled", "");
-  document.querySelector(".btn-edit").removeAttribute("disabled", "");
-  modifyTxt.innerHTML = defaultValue;
+    document.querySelectorAll(".todo-lst li").forEach( e => {
+      const txt = e.children[0];
+      const btn = e.children[1];
+      txt.innerHTML = txt.innerText;
+      if (!e.classList.contains("completed")) {
+        btn.classList.remove("hide");
+      }
+    });
+  }
+
+  // 리스트 항목 삭제
+  const btnDel = document.querySelector('.btn-delete');
+  btnDel.addEventListener('click', function (e) {
+    let chk = false;
+
+    document.querySelectorAll("input[type=checkbox]").forEach(e => {
+      if (e.checked === true) {
+        chk = true;
+        document.querySelector(".todo-lst").removeChild(e.parentNode.parentNode.parentNode);
+      }
+    });
+
+    if (!chk) {
+      alert("선택된 항목이 없습니다.");
+    } else {
+      chkboxHide();
+      alert("선택한 항목이 삭제 되었습니다.");
+    }
+  });
+
+  // 리스트 항목 완료
+  const btnComp = document.querySelector('.btn-comp');
+  btnComp.addEventListener('click', function () {
+    let chk = false;
+
+    document.querySelectorAll("input[type=checkbox]").forEach(e => {
+      if (e.checked === true) {
+        chk = true;
+        e.parentNode.parentNode.parentNode.classList.add("completed");
+      }
+    });
+
+    if (!chk) {
+      alert("선택된 항목이 없습니다.");
+    } else {
+      chkboxHide();
+      alert("선택한 항목이 완료 되었습니다.");
+    }
+  });
+
+  // 리스트 항목 진행
+  const btnActive = document.querySelector('.btn-active');
+  btnActive.addEventListener('click', function () {
+    let chk = false;
+
+    document.querySelectorAll("input[type=checkbox]").forEach(e => {
+      if (e.checked === true) {
+        chk = true;
+        e.parentNode.parentNode.parentNode.classList.remove("completed");
+      }
+    });
+
+    if (!chk) {
+      alert("선택된 항목이 없습니다.");
+    } else {
+      chkboxHide();
+      alert("선택한 항목이 진행 상태로 전환 되었습니다.");
+    }
+  });
+
+  // 리스트 수정
+  let defaultValue;
+  
+  const btnModify = document.querySelectorAll('.btn-modify');
+  
+  btnModify.forEach(e => {
+    e.addEventListener('click', function (e) {
+      const btnTarget = e.target;
+      const btnWrap = btnTarget.parentNode.parentNode;
+      const btnEdit = document.querySelector(".btn-edit");
+      const modifyTxt = btnTarget.parentNode.parentNode.children[0];
+      defaultValue = modifyTxt.innerText;
+
+      if (!flag) {
+        flag = true;
+        btnTarget.innerText = "저장";
+        
+        btnWrap.classList.add("modify");
+        btnEdit.removeAttribute("enabled", "");
+        btnEdit.setAttribute("disabled", "");
+
+        // btnTarget.parentNode.innerHTML += `<button class="btn-cancle">취소</button>`;
+        modifyTxt.innerHTML = '<input type="text" value="' + modifyTxt.innerText + '" />';
+      } else {
+        flag = false;
+        btnTarget.innerText = "수정";
+        // btnTarget.nextElementSibling.remove();
+        btnWrap.classList.remove("modify");
+        btnEdit.setAttribute("enabled", "");
+        btnEdit.removeAttribute("disabled", "");
+        modifyTxt.innerHTML = btnWrap.querySelector("input").value;
+      }
+    });
+  });
+
+  // const btnCancle = document.querySelector('.btn-cancle');
+  // btnCancle.addEventListener('click', function (e) {
+  //   console.log("6463")
+  //   const btnTarget = e.target;
+    // const btnWrap = btnTarget.parentNode.parentNode;
+    // const modifyTxt = btnWrap.children[0];
+    // flag = false;
+    // btnTarget.previousElementSibling.innerText = "수정";
+    // btnWrap.classList.remove("modify")
+    // btnTarget.remove();
+    // document.querySelector(".btn-edit").setAttribute("enabled", "");
+    // document.querySelector(".btn-edit").removeAttribute("disabled", "");
+    // modifyTxt.innerHTML = defaultValue;
+  // });
 }
