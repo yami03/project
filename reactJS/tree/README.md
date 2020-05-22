b84832182d353e02fb3c2576ff06e5580b13d49b
mook <mook9288@gmail.com>
2020-05-23 02:01:21 +0900

    add.[기본 구조 퍼블리싱]

--- reactJS/tree/README.md
+++ reactJS/tree/README.md
@@ -1,68 +1,27 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# React Tree
 
-## Available Scripts
+## 샘플
+- 트리구조를 활용한 브라우저의 북마크 관리 기능 구현
 
-In the project directory, you can run:
+## 기능 구현
+### 새폴더 추가
+- 폴더 선택 후 새폴터 버튼 클릭시 선택된 폴더 하위에 새폴더 추가
+- 선택한 폴더 없이 새폴더 버튼 클릭 시 최상위 레벨에 새폴더 추가
+- 해당 폴더 우클릭-메뉴-하위에 새폴더 추가 클릭시 하위에 새폴더 추가
 
-### `yarn start`
+### 폴더명 마우스 우클릭
+- Context Menu 노출
+- 폴더 이름 변경(Input/확인/취소)
+- 하위에 새폴더 추가(새폴더 추가 기능)
+- 폴더 삭제(alert 경고 후 폴더 속 내용 전부 삭제)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### 폴더 아이콘 앞 화살표
+- 하위에 폴더 구조가 있으면 화살표 표시, 없으면 화살표 없음
+- 화살표로 폴더 접고 펼치기(이때 폴더 아이콘 모양은 유지)
+- 기본 전체 열림/닫힘 선택
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+### 폴더 List Click 이벤트
+- 폴더명을 클릭했을 때 폴더가 선택, FontColor 변경, 폴더 아이콘 open으로 변경
+- 폴더명 선택 활성화는 하나만 가능
+- 오른쪽에 폴더 내용 노출(하위폴더 노출 여부 선택)
+- Drag and Drop
--- reactJS/tree/src/App.js
+++ reactJS/tree/src/App.js
@@ -1,26 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; 
+import { Button, Icon, Input, Header, Segment, Grid } from 'semantic-ui-react'
+import './style.scss';
+// import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Segment>
+        <Header>
+          <Header.Content>React Tree Project</Header.Content>
+          <Header.Subheader textAlign="right">
+            <Input placehelder="검색" /><Button content="검색" />
+          </Header.Subheader>
+        </Header>
+        <Grid>
+          <Grid.Column width={6}>
+            <div className="tree-wrapper">
+              <ul>
+                <li className="tree-node tree-node-parent tree-node-expanded">
+                  <div className="tree-text">
+                    <div className="tree-func tree-collapse">
+                      <Button
+                        className="tree-collapse-btn"
+                        icon="caret down"
+                      />
+                    </div>
+                    <div className="tree-bare-label">
+                      <Icon name="folder" />
+                      <div>트리구조</div>
+                    </div>
+                  </div>
+                  <ul>
+                    <li className="tree-node tree-node-parent tree-node-expanded">
+                      <div className="tree-text">
+                        <div className="tree-func tree-collapse">
+                          <Button
+                            className="tree-collapse-btn"
+                            icon="caret down"
+                          />
+                        </div>
+                        <div className="tree-bare-label tree-node-selected">
+                          <Icon name="folder" />
+                          <div>트리구조</div>
+                        </div>
+                      </div>
+                      <ul>
+                        <li className="tree-node rct-node-leaf">
+                          <div className="tree-text">
+                            <div className="tree-func tree-collapse">
+                              <Button
+                                className="tree-collapse-btn"
+                                icon="caret down"
+                              />
+                            </div>
+                            <div className="tree-bare-label">
+                              <Icon name="folder" />
+                              <div>트리구조</div>
+                            </div>
+                          </div>
+                        </li>
+                        <li className="tree-node rct-node-leaf">
+                          <div className="tree-text">
+                            <div className="tree-func tree-collapse">
+                              <Button
+                                className="tree-collapse-btn"
+                                icon="caret down"
+                              />
+                            </div>
+                            <div className="tree-bare-label">
+                              <Icon name="folder" />
+                              <div>
+                                <Input />
+                                <Button basic>확인 </Button>
+                                <Button basic>취소 </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="tree-node tree-node-parent tree-node-collapsed">
+                      <div className="tree-text">
+                        <div className="tree-func tree-collapse">
+                          <Button
+                            className="tree-collapse-btn"
+                            icon="caret right"
+                          />
+                        </div>
+                        <div className="tree-bare-label">
+                          <Icon name="folder" />
+                          <div>
+                            <Input />
+                            <Button basic>확인 </Button>
+                            <Button basic>취소 </Button>
+                          </div>
+                        </div>
+                      </div>
+                      <ul>
+                        <li className="tree-node rct-node-leaf">
+                          <div className="tree-text">
+                            <div className="tree-func tree-collapse">
+                              <Button
+                                className="tree-collapse-btn"
+                                icon="caret down"
+                              />
+                            </div>
+                            <div className="tree-bare-label">
+                              <Icon name="folder" />
+                              <div>트리구조</div>
+                            </div>
+                          </div>
+                        </li>
+                        <li className="tree-node rct-node-leaf">
+                          <div className="tree-text">
+                            <div className="tree-func tree-collapse">
+                              <Button
+                                className="tree-collapse-btn"
+                                icon="caret down"
+                              />
+                            </div>
+                            <div className="tree-bare-label">
+                              <Icon name="folder" />
+                              <div>
+                                <Input />
+                                <Button basic>확인 </Button>
+                                <Button basic>취소 </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li className="tree-node rct-node-leaf">
+                  <div className="tree-text">
+                    <div className="tree-func tree-collapse">
+                      <Button
+                        className="tree-collapse-btn"
+                        icon="caret down"
+                      />
+                    </div>
+                    <div className="tree-bare-label">
+                      <Icon name="folder" />
+                      <div>
+                        <Input />
+                        <Button basic>확인 </Button>
+                        <Button basic>취소 </Button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <div className="page-list">
+              <ul>
+                <li className="item">
+                  <Icon name="desktop" />text text text text texttext text texttext text
+                  <Button className="closed" />
+                </li>
+              </ul>
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
 }
 
 export default App;
